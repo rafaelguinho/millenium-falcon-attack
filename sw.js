e370a95d8cd3ab0a4879f5b756cd4dd6fbfc547e
Rafael Guinho Monteiro <rafaelguinho@outlook.com>
2016-06-22 13:29:31 -0300

    Improvements on sw

--- sw.js
+++ sw.js
@@ -3,18 +3,15 @@ self.addEventListener('install', function(event) {
     caches.open('v5').then(function(cache) {
       return cache.addAll([
         'index.html',
-        'style/',
         'style/fonts.css',
         'style/game.css',
         'style/reset.css',
-        'fonts/',
         'fonts/lcd_solid-webfont.eot',
         'fonts/lcd_solid-webfont.svg',
         'fonts/lcd_solid-webfont.ttf',
         'fonts/lcd_solid-webfont.woff',
         'fonts/lcd_solid-webfont.woff2',
         'fonts/Starjedi.ttf',
-        'img/',
         'img/asteroid.png',
         'img/botao-jogar.png',
         'img/credits.svg',
@@ -25,15 +22,14 @@ self.addEventListener('install', function(event) {
         'img/sprite-nave.png',
         'img/start.svg',
         'img/tie.png',
-        'snd/',
         'snd/explosao.mp3',
         'snd/musica-acao.mp3',
         'snd/tiro.mp3',
-        'dist/',
         'dist/all.js',
         'launcher-icon-2x.png',
         'launcher-icon-3x.png',
-        'launcher-icon-4x.png'
+        'launcher-icon-4x.png',
+        'favicon-16x16.png'
       ]);
     })
   );
