802f28a192f665ddebc8ad3c9fc9c66c20f63158
rafaelguinho@outlook.com <rafaelguinho@outlook.com>
2016-09-23 15:59:44 -0800

    Now

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
 
-    caches.open('v10').then(function(cache) {
+    caches.open('v11').then(function(cache) {
       return cache.addAll([
+        '',
+        '/',
         'index.html',
         'credits.html',
         'style/fonts.css',
@@ -37,9 +39,11 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
+  event.respondWith(caches.match(event.request).then(function(response){
+      if(response)
+        return response;
+      return fetch(event.request).then(function(response){
+        return response;
+      });
+  }));
 });
